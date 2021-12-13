d6cd59050de91606fdec8af6052f87398637e956
Matthew Wong <matthew.w1996@hotmail.com>
2021-12-13 17:32:59 +0800

    backend init

---
+++ backend/main.ts
@@ -0,0 +1,9 @@
+import express from "express";
+
+const app = express();
+
+const PORT = process.env.PORT ?? 8080;
+
+app.listen(PORT, () => {
+    console.log(`[info] listening to port ${PORT}`)
+})

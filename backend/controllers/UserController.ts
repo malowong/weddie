46145396d65b3a14e002ba44a22ed18a4f5ac759
Dennis Chan <dennisming2012@gmail.com>
2021-12-15 23:24:50 +0800

    .

--- backend/controllers/UserController.ts
+++ backend/controllers/UserController.ts
@@ -1,8 +1,15 @@
 import { UserService } from "../services/UserService";
 import { Request, Response } from "express";
+import { logger } from "../utils/logger";
 
 export class UserController {
+  private readonly tag = "UserController";
   constructor(private userService: UserService) {}
 
-  login = async (req: Request, res: Response) => {};
+  login = async (req: Request, res: Response) => {
+    try {
+    } catch (err) {
+      logger.error(`[${this.tag}] ${err.message}`);
+    }
+  };
 }

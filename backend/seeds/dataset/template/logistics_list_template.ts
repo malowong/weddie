e0342c562059a593cb80b0062d2d7013c0eec189
Billy <billychu25@gmail.com>
2021-12-24 22:53:17 +0800

    same as last time

---
+++ backend/seeds/dataset/template/logistics_list_template.ts
@@ -0,0 +1,134 @@
+export const logistics_list_template = [
+  {
+    logistics_item_temp: "物品",
+  },
+  {
+    logistics_item_temp: "身份證",
+  },
+  {
+    logistics_item_temp: "手提電話",
+  },
+  {
+    logistics_item_temp: "結婚戒子",
+  },
+  {
+    logistics_item_temp: "開門利是",
+  },
+  {
+    logistics_item_temp: "工作人員利是",
+  },
+  {
+    logistics_item_temp: "金器袋、金器、金器盒",
+  },
+  {
+    logistics_item_temp: "主婚人襟花",
+  },
+  {
+    logistics_item_temp: "絲花襟花及手花 (後備)",
+  },
+  {
+    logistics_item_temp: "茶葉、蓮子、紅棗、紅杯仔",
+  },
+  {
+    logistics_item_temp: "Nude Bra",
+  },
+  {
+    logistics_item_temp: "修肚褲",
+  },
+  {
+    logistics_item_temp: "便服及鞋",
+  },
+  {
+    logistics_item_temp: "藥 (頭痛, 鼻敏感, 皮膚)",
+  },
+  {
+    logistics_item_temp: "卸妝水, 化妝棉",
+  },
+  {
+    logistics_item_temp: "眼藥水, 膠布",
+  },
+  {
+    logistics_item_temp: "濕紙巾",
+  },
+  {
+    logistics_item_temp: "雙面膠紙, 扣針, 針線",
+  },
+  {
+    logistics_item_temp: "棉花棒, Cream",
+  },
+  {
+    logistics_item_temp: "飲管",
+  },
+  {
+    logistics_item_temp: "原子筆 / 油性Marker",
+  },
+  {
+    logistics_item_temp: "Label",
+  },
+  {
+    logistics_item_temp: "A4紙",
+  },
+  {
+    logistics_item_temp: "雙面膠紙",
+  },
+  {
+    logistics_item_temp: "膠紙 / 皺紋膠紙",
+  },
+  {
+    logistics_item_temp: "𠝹刀",
+  },
+  {
+    logistics_item_temp: "較剪",
+  },
+  {
+    logistics_item_temp: "繩",
+  },
+  {
+    logistics_item_temp: "Blu-Tack",
+  },
+  {
+    logistics_item_temp: "結婚證書套 (Tiffany Blue)",
+  },
+  {
+    logistics_item_temp: "証婚簽名筆",
+  },
+  {
+    logistics_item_temp: "金銀簽名筆 x6",
+  },
+  {
+    logistics_item_temp: "3'x 2' 大油畫 x1",
+  },
+  {
+    logistics_item_temp: '12" x 12" 小油畫 x 3',
+  },
+  {
+    logistics_item_temp: "禮物袋 x 2 + Label + 人情袋 x 2(收禮用)",
+  },
+  {
+    logistics_item_temp: "佈置公仔",
+  },
+  {
+    logistics_item_temp: "賓客名單 x6",
+  },
+  {
+    logistics_item_temp: "利是封 (備用)",
+  },
+  {
+    logistics_item_temp: "簽名布",
+  },
+  {
+    logistics_item_temp: "手指 (音響、成長片段)",
+  },
+  {
+    logistics_item_temp: "隱形眼鏡",
+  },
+  {
+    logistics_item_temp: "無鏡片眼鏡",
+  },
+  {
+    logistics_item_temp: "新娘媽媽晚裝 ",
+  },
+  {
+    logistics_item_temp: "新郎媽媽晚裝",
+  },
+];
---
+++ backend/seeds/dataset/template/to_do_list_template.ts
@@ -0,0 +1,282 @@
+export const to_do_list_template = [
+  {
+    to_do_temp: "安排雙方家長會面",
+    days_prior_wedding: "365",
+  },
+  {
+    to_do_temp: "決定婚禮形式及行禮地點",
+    days_prior_wedding: "366",
+  },
+  {
+    to_do_temp: "商量聘禮及禮金數目",
+    days_prior_wedding: "367",
+  },
+  {
+    to_do_temp: "決定婚宴日期及地點",
+    days_prior_wedding: "368",
+  },
+  {
+    to_do_temp: "擇定結婚日期",
+    days_prior_wedding: "369",
+  },
+  {
+    to_do_temp: "預訂酒席",
+    days_prior_wedding: "370",
+  },
+  {
+    to_do_temp: "制定婚禮開支預算",
+    days_prior_wedding: "371",
+  },
+  {
+    to_do_temp: "美容、美髮及皮膚護理計劃",
+    days_prior_wedding: "372",
+  },
+  {
+    to_do_temp: "新居租置或選購計劃",
+    days_prior_wedding: "373",
+  },
+  {
+    to_do_temp: "洽借教堂",
+    days_prior_wedding: "180",
+  },
+  {
+    to_do_temp: "搜集及落實攝錄攝影公司",
+    days_prior_wedding: "181",
+  },
+  {
+    to_do_temp: "搜集及落實婚紗店,裙褂店",
+    days_prior_wedding: "182",
+  },
+  {
+    to_do_temp: "搜集及落實場地佈置公司",
+    days_prior_wedding: "183",
+  },
+  {
+    to_do_temp: "搜集及落實化妝師店",
+    days_prior_wedding: "184",
+  },
+  {
+    to_do_temp: "拍攝婚紗照",
+    days_prior_wedding: "185",
+  },
+  {
+    to_do_temp: "設計及印製喜帖",
+    days_prior_wedding: "90",
+  },
+  {
+    to_do_temp: "排期註冊",
+    days_prior_wedding: "91",
+  },
+  {
+    to_do_temp: "辦理擬結婚通知書",
+    days_prior_wedding: "92",
+  },
+  {
+    to_do_temp: "列出婚禮及婚宴邀客名單",
+    days_prior_wedding: "93",
+  },
+  {
+    to_do_temp: "訂造結婚戒指",
+    days_prior_wedding: "94",
+  },
+  {
+    to_do_temp: "訂造或預租晚裝",
+    days_prior_wedding: "95",
+  },
+  {
+    to_do_temp: "選新郎禮服，預租裙褂",
+    days_prior_wedding: "96",
+  },
+  {
+    to_do_temp: "替雙方家長訂造禮服",
+    days_prior_wedding: "97",
+  },
+  {
+    to_do_temp: "婚前檢查",
+    days_prior_wedding: "98",
+  },
+  {
+    to_do_temp: "計劃蜜月旅行地點及財政預算",
+    days_prior_wedding: "99",
+  },
+  {
+    to_do_temp: "組織婚禮籌備委員會分配職位及工作",
+    days_prior_wedding: "100",
+  },
+  {
+    to_do_temp: "裝修新居",
+    days_prior_wedding: "101",
+  },
+  {
+    to_do_temp: "準備嫁粧",
+    days_prior_wedding: "60",
+  },
+  {
+    to_do_temp: "預約花車",
+    days_prior_wedding: "61",
+  },
+  {
+    to_do_temp: "預備回禮小禮品",
+    days_prior_wedding: "62",
+  },
+  {
+    to_do_temp: "計劃婚禮場地佈置",
+    days_prior_wedding: "63",
+  },
+  {
+    to_do_temp: "試粧，試頭",
+    days_prior_wedding: "64",
+  },
+  {
+    to_do_temp: "召開籌委會會議",
+    days_prior_wedding: "65",
+  },
+  {
+    to_do_temp: "準備過大禮物品",
+    days_prior_wedding: "66",
+  },
+  {
+    to_do_temp: "擬定新居購物清單",
+    days_prior_wedding: "67",
+  },
+  {
+    to_do_temp: "選照片作成長片段、選婚禮歌曲",
+    days_prior_wedding: "68",
+  },
+  {
+    to_do_temp: "準備婚宴用品",
+    days_prior_wedding: "36",
+  },
+  {
+    to_do_temp: "過大禮",
+    days_prior_wedding: "35",
+  },
+  {
+    to_do_temp: "製訂結婚程序表及座位表",
+    days_prior_wedding: "34",
+  },
+  {
+    to_do_temp: "最後確定嘉賓及工作人員名單",
+    days_prior_wedding: "33",
+  },
+  {
+    to_do_temp: "試穿婚紗，晚裝，裙褂，禮服",
+    days_prior_wedding: "32",
+  },
+  {
+    to_do_temp: "編訂教堂禮儀",
+    days_prior_wedding: "31",
+  },
+  {
+    to_do_temp: "寄發請帖",
+    days_prior_wedding: "30",
+  },
+  {
+    to_do_temp: "逐步遷入新居",
+    days_prior_wedding: "30",
+  },
+  {
+    to_do_temp: "覆實化粧師，髮型師，攝影師",
+    days_prior_wedding: "18",
+  },
+  {
+    to_do_temp: "執拾家居用品及安排搬入新居",
+    days_prior_wedding: "17",
+  },
+  {
+    to_do_temp: "通知酒樓嘉賓人數,確定婚宴菜式",
+    days_prior_wedding: "16",
+  },
+  {
+    to_do_temp: "婚禮授懽書送交神父/牧師",
+    days_prior_wedding: "15",
+  },
+  {
+    to_do_temp: "繼續召開會議，觀察工作進度",
+    days_prior_wedding: "14",
+  },
+  {
+    to_do_temp: "在教堂行禮，作婚前綵排",
+    days_prior_wedding: "10",
+  },
+  {
+    to_do_temp: "舉行戥穿石聚會",
+    days_prior_wedding: "9",
+  },
+  {
+    to_do_temp: "舉行姊妹團午宴",
+    days_prior_wedding: "8",
+  },
+  {
+    to_do_temp: "預訂回門燒豬",
+    days_prior_wedding: "7",
+  },
+  {
+    to_do_temp: "向家長及各工作人員講解結婚當日程序",
+    days_prior_wedding: "2",
+  },
+  {
+    to_do_temp: "寫台上多謝家人朋友台詞",
+    days_prior_wedding: "2",
+  },
+  {
+    to_do_temp: "收拾婚禮及婚宴物品,順序排列",
+    days_prior_wedding: "2",
+  },
+  {
+    to_do_temp: "最後統計婚宴坐位表",
+    days_prior_wedding: "2",
+  },
+  {
+    to_do_temp: "往美容院護理皮膚、GEL甲",
+    days_prior_wedding: "2",
+  },
+  {
+    to_do_temp: "預備利是,貼士和各項費用",
+    days_prior_wedding: "2",
+  },
+  {
+    to_do_temp: "執拾旅行裝備，證件",
+    days_prior_wedding: "2",
+  },
+  {
+    to_do_temp: "預訂酒樓午膳",
+    days_prior_wedding: "2",
+  },
+  {
+    to_do_temp: "提醒攝影師,化粧師,髮型師",
+    days_prior_wedding: "1",
+  },
+  {
+    to_do_temp: "收拾婚禮及婚宴物品,順序排列",
+    days_prior_wedding: "1",
+  },
+  {
+    to_do_temp: "預備敬茶用的茶具和全盒",
+    days_prior_wedding: "1",
+  },
+  {
+    to_do_temp: "分發婚禮程序表及座位編排圖表",
+    days_prior_wedding: "1",
+  },
+  {
+    to_do_temp: "由大妗姐或長者為新郎新娘(上頭)",
+    days_prior_wedding: "1",
+  },
+  {
+    to_do_temp: "預備利是,貼士和各項費用",
+    days_prior_wedding: "1",
+  },
+  {
+    to_do_temp: "取花球襟花",
+    days_prior_wedding: "1",
+  },
+  {
+    to_do_temp: "取婚紗禮服",
+    days_prior_wedding: "1",
+  },
+  {
+    to_do_temp: "充足睡眠迎接大喜日子",
+    days_prior_wedding: "1",
+  },
+];

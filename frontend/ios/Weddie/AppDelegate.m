be33aa65ef8d4b86aaa509591df45853c4bac908
Matthew Wong <matthew.w1996@hotmail.com>
2022-01-03 02:38:03 +0800

    changed app name and added icon

--- frontend/ios/Weddie/AppDelegate.m
+++ frontend/ios/Weddie/AppDelegate.m
@@ -33,7 +33,7 @@ static void InitializeFlipper(UIApplication *application) {
 
   RCTBridge *bridge = [[RCTBridge alloc] initWithDelegate:self launchOptions:launchOptions];
   RCTRootView *rootView = [[RCTRootView alloc] initWithBridge:bridge
-                                                   moduleName:@"AwesomeTSProject2"
+                                                   moduleName:@"Weddie"
                                             initialProperties:nil];
 
   if (@available(iOS 13.0, *)) {
--- frontend/ios/WeddieTests/WeddieTests.m
+++ frontend/ios/WeddieTests/WeddieTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface AwesomeTSProject2Tests : XCTestCase
+@interface WeddieTests : XCTestCase
 
 @end
 
-@implementation AwesomeTSProject2Tests
+@implementation WeddieTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
